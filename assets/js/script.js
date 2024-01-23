b7bf12073d938fd9d3625c935defce32f832334a
Constantin <horghidanconstantin91@yahoo.com>
2024-01-23 22:44:11 +0000

    added more comments to Js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Extend dayjs with advanced format plugin
 dayjs.extend(window.dayjs_plugin_advancedFormat);
 
-// Get the current date
+// Get the current date using dayjs
 const currentDate = dayjs();
 // Display the current date on the page
 $('#currentDay').text(currentDate.format('dddd, MMMM Do'));
@@ -9,17 +9,22 @@ $('#currentDay').text(currentDate.format('dddd, MMMM Do'));
 // Set the start and finish time for the day
 const workStartHour = 9;
 const workEndHour = 17;
+// Declare variable to store daily tasks
 let dailyTasks;
 
 // Retrieve tasks from localStorage
 if ((dailyTasks = localStorage.getItem('dailyTasks'))) {
+  // Parse JSON data if tasks exist in local storage
   dailyTasks = JSON.parse(dailyTasks);
 } else {
+   // Initialize an empty object for daily tasks if no tasks are found in local storage
   dailyTasks = {};
 }
 // Loop through each work hour block from start to finish
 for (let i = workStartHour; i <= workEndHour; i++) {
+  // Get the formatted label for the current hour
     let hourLabel = dayjs().hour(i).format('hA');
+    // Determine the status of the hour (past, present, or future) based on the current time
     let hourStatus;
 
     if (currentDate.hour() < i) {
@@ -29,9 +34,10 @@ for (let i = workStartHour; i <= workEndHour; i++) {
     } else {
       hourStatus = 'present';
     }
-
+    // Get the jQuery element for the current hour block
     let $timeBlock = $(`#${i}am`);
 
+    // Check if the time block already exists
     if ($timeBlock.length > 0) {
         // Time block already exists, update its content and apply color-coding
         $timeBlock.find('.hour').text(hourLabel);
@@ -52,14 +58,14 @@ $('.saveBtn').on('click', function () {
   // Return if no change was detected
   if (userInput === dailyTasks[index]) return;
 
-  // Update dailyTasks object and save into storage
+  // Update dailyTasks object with user input and save into local storage
   dailyTasks[index] = userInput;
 
-  // Stringify and store into local storage
+   // Stringify and store dailyTasks into local storage
   const tasksData = JSON.stringify(dailyTasks);
   localStorage.setItem('dailyTasks', tasksData);
 
-// Log dailyTasks object and localStorage
+// Log updated dailyTasks object and current state of local storage
   console.log('dailyTasks:', dailyTasks);
   console.log('localStorage:', localStorage);
 });
